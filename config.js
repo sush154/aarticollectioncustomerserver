5f223aebf5b1166e89ed49740df7bb0000e4cc94
sush154 <sushant.pj41288@gmail.com>
2017-12-22 19:04:36 +0530

    Office commit

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ var allowedOrigins = ['http://localhost:3000', 'http://localhost:8100'];
 
 var config = {
 		mongo : {
-			connectionUrl : 'mongodb://admin:admin@ds163053.mlab.com:63053/aartiscollection'
-			//connectionUrl : 'mongodb://localhost:27017/aartiscollection'
+			//connectionUrl : 'mongodb://admin:admin@ds163053.mlab.com:63053/aartiscollection'
+			connectionUrl : 'mongodb://localhost:27017/aartiscollection'
 		},
 		mongooseConnection : null,
 		client : {
